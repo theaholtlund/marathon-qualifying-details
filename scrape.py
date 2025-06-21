9d301ab3c4afb45faa59376d6df9e0ffabd76636
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2025-06-21 13:05:49 +0100

    Create initial file for scraping

---
+++ scrape.py
@@ -0,0 +1,12 @@
+# Import required libraries
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_5) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/122.0.0.0 Safari/537.36"
+    )
+}
