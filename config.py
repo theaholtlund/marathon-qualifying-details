1f944015162872d40c3a19bb8168c25452be34c3
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2025-06-21 13:01:05 +0100

    Add initial config

---
+++ config.py
@@ -0,0 +1,14 @@
+# Import required libraries
+import os
+from dotenv import load_dotenv
+import pyodbc
+
+# Load environment variables from env file
+load_dotenv()
+
+def get_db_connection():
+    conn_str = os.getenv("SQL_CONNECTION_STRING")
+    if not conn_str:
+        raise ValueError("SQL_CONNECTION_STRING not set in environment")
+    conn = pyodbc.connect(conn_str)
+    return conn
